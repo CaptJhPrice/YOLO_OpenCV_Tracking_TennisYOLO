37ecf38f535a72a00af951aa311cb3ae857916fa
Prabal Singh <141550161+CaptJhPrice@users.noreply.github.com>
2026-01-03 18:35:19 +0530

    Update video_utils.py

--- tennis_utils/video_utils.py
+++ tennis_utils/video_utils.py
@@ -11,6 +11,7 @@ def read_video(video_path):
     cap.release()
     return frames
 
+
 def save_video(output_video_frames, output_video_path):
     fourcc = cv2.VideoWriter_fourcc(*'MJPG')
     out = cv2.VideoWriter(output_video_path, fourcc, 24, (output_video_frames[0].shape[1], output_video_frames[0].shape[0]))
